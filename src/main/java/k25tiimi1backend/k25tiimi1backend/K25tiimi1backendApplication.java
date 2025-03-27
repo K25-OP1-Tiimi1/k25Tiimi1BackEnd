898a7443ae4e7cfcf309fe82f47436f83392236e
Niko Kaarmila <bha523@myy.haaga-helia.fi>
2025-03-27 14:35:04 +0200

    First commit

---
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/K25tiimi1backendApplication.java
@@ -0,0 +1,13 @@
+package k25tiimi1backend.k25tiimi1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K25tiimi1backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K25tiimi1backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/k25tiimi1backend/k25tiimi1backend/K25tiimi1backendApplicationTests.java
@@ -0,0 +1,13 @@
+package k25tiimi1backend.k25tiimi1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K25tiimi1backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
