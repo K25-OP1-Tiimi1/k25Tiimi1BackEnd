aaeba932e5701406682317f0f246f3d7ef782951
Niko Kaarmila <bha523@myy.haaga-helia.fi>
2025-04-01 11:36:21 +0300

    Edit and delete added

--- src/main/java/k25tiimi1backend/k25tiimi1backend/web/ProductController.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/web/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import k25tiimi1backend.k25tiimi1backend.domain.Product;
@@ -28,16 +29,30 @@ public class ProductController {
     }
 
     @GetMapping("/add")
-    public String addProduct(Model model){
-    	model.addAttribute("product", new Product());
-        model.addAttribute("valmistaja", valmistajaRepository.findAll());
+    public String addProduct(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("valmistajaList", valmistajaRepository.findAll());
         return "addproduct";
-    }  
-
+    }
+    
     @PostMapping("/save")
     public String saveProduct(@ModelAttribute Product product) {
         productRepository.save(product);
         return "redirect:/productlist";
     }
     
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long productId) {
+        productRepository.deleteById(productId);
+        return "redirect:/productlist";
+}
+
+@GetMapping("/edit/{id}")
+public String editProduct(@PathVariable("id") Long id, Model model) {
+    Product product = productRepository.findById(id).orElse(null); 
+    model.addAttribute("product", product);
+    model.addAttribute("valmistajat", valmistajaRepository.findAll());
+
+    return "editproduct";
+}
 }
