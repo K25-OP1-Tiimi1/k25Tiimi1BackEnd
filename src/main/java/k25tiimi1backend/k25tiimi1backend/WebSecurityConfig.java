5a10ca8fb69b714989586ee59371c1c8b452ac15
RistoHaime <risto.haime@myy.haaga-helia.fi>
2025-04-14 21:10:08 +0300

    lisätty /products sallittujen listalle ja otettu admin tunnusten vaatiminen demoa varten käyttöön

--- src/main/java/k25tiimi1backend/k25tiimi1backend/WebSecurityConfig.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/WebSecurityConfig.java
@@ -3,48 +3,47 @@ package k25tiimi1backend.k25tiimi1backend;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-/*
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-*/
-import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
-        /*
-         * http
-         * .authorizeHttpRequests(authorize -> authorize
-         * .anyRequest().authenticated())
-         * .formLogin(formlogin -> formlogin
-         * .defaultSuccessUrl("/", true)
-         * .permitAll())
-         * .logout(logout -> logout
-         * .permitAll());
-         * return http.build();
-         */
+       // return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
+        
+          http
+          .authorizeHttpRequests(authorize -> authorize
+          .requestMatchers(antMatcher("/products")).permitAll()
+          .anyRequest().authenticated())
+          .formLogin(formlogin -> formlogin
+          .defaultSuccessUrl("/", true)
+          .permitAll())
+          .logout(logout -> logout
+          .permitAll());
+          return http.build();
+         
     }
-    /*
-     * @Bean
-     * public UserDetailsService userDetailsService() {
-     * List<UserDetails> users = new ArrayList<UserDetails>();
-     * UserDetails user = User.withDefaultPasswordEncoder()
-     * .username("admin")
-     * .password("password")
-     * .roles("ADMIN")
-     * .build();
-     * 
-     * users.add(user);
-     * 
-     * return new InMemoryUserDetailsManager(users);
-     * }
-     */
+    
+      @Bean
+      public UserDetailsService userDetailsService() {
+      List<UserDetails> users = new ArrayList<UserDetails>();
+      UserDetails user = User.withDefaultPasswordEncoder()
+      .username("admin")
+      .password("password")
+      .roles("ADMIN")
+      .build();
+      
+      users.add(user);
+      
+      return new InMemoryUserDetailsManager(users);
+      }
+     
 }
