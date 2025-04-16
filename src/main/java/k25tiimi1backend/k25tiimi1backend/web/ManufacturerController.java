a42417fb17e9a2e58f14418f75b5a121d7f913d5
denwell76 <danielnieminen04@gmail.com>
2025-04-16 21:09:11 +0300

    Valmistajien poistaminen onnistuu nyt

--- src/main/java/k25tiimi1backend/k25tiimi1backend/web/ManufacturerController.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/web/ManufacturerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import k25tiimi1backend.k25tiimi1backend.domain.Manufacturer;
@@ -35,6 +36,14 @@ public class ManufacturerController {
         manufacturerRepository.save(manufacturer);
         return "redirect:/manufacturerlist";
     }
+
+    @GetMapping("/deletemanufacturer/{id}")
+    public String deleteManufacturer(@PathVariable("id") Long manufacturerId) {
+    manufacturerRepository.deleteById(manufacturerId);
+    return "redirect:/manufacturerlist";
+}
+
+    
 
 
 
