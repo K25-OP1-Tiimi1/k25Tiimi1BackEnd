7157628bc03c6d09d59849c9078386de74590993
jussilampinen <bhi090@myy.haaga-helia.fi>
2025-04-22 14:46:30 +0300

    .

--- src/main/java/k25tiimi1backend/k25tiimi1backend/WebSecurityConfig.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/WebSecurityConfig.java
@@ -3,7 +3,7 @@ package k25tiimi1backend.k25tiimi1backend;
 import java.util.ArrayList;
 import java.util.List;
 
-// import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,38 +13,44 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 public class WebSecurityConfig {
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll()).build();
-        
-      //     http
-      //     .authorizeHttpRequests(authorize -> authorize
-      //     .requestMatchers(antMatcher("/products")).permitAll()
-      //     .requestMatchers(antMatcher("/users")).permitAll()
-      //     .anyRequest().authenticated())
-      //     .formLogin(formlogin -> formlogin
-      //     .defaultSuccessUrl("/", true)
-      //     .permitAll())
-      //     .logout(logout -> logout
-      //     .permitAll());
-      //     return http.build();
-         
-    }
-    
-      @Bean
-      public UserDetailsService userDetailsService() {
-      List<UserDetails> users = new ArrayList<UserDetails>();
-      UserDetails user = User.withDefaultPasswordEncoder()
-      .username("admin")
-      .password("password")
-      .roles("ADMIN")
-      .build();
-      
-      users.add(user);
-      
-      return new InMemoryUserDetailsManager(users);
-      }
-     
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    // return http.authorizeHttpRequests((auth) ->
+    // auth.anyRequest().permitAll()).build();
+
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(antMatcher("/api/**")).permitAll()
+            // .requestMatchers(antMatcher("/users")).permitAll()
+            .requestMatchers(antMatcher("/css/**")).permitAll()
+            .requestMatchers(antMatcher("/h2-console/**")).permitAll()
+            .anyRequest().authenticated())
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+        .formLogin(formlogin -> formlogin
+            .defaultSuccessUrl("/", true)
+            .permitAll())
+        .logout(logout -> logout
+            .permitAll())
+        .csrf(csrf -> csrf.disable());
+    return http.build();
+
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    List<UserDetails> users = new ArrayList<UserDetails>();
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("admin")
+        .password("password")
+        .roles("ADMIN")
+        .build();
+
+    users.add(user);
+
+    return new InMemoryUserDetailsManager(users);
+  }
+
 }
--- src/main/java/k25tiimi1backend/k25tiimi1backend/web/UserRestController.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/web/UserRestController.java
@@ -10,16 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import k25tiimi1backend.k25tiimi1backend.domain.User;
 import k25tiimi1backend.k25tiimi1backend.domain.UserRepository;
 
 @RestController
-//@RequestMapping("/api")
-@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge=3600)
+@RequestMapping("/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 public class UserRestController {
-
 
     @Autowired
     private UserRepository userRepository;
@@ -30,15 +30,15 @@ public class UserRestController {
     }
 
     @GetMapping("/user/{id}")
-    public Optional<User> getUserById(@PathVariable("id") Long id){
+    public Optional<User> getUserById(@PathVariable("id") Long id) {
         return userRepository.findById(id);
     }
 
     @PostMapping("/registerUser")
-    public User newUser(@RequestBody User newUser){
+    public User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
-    
+
     @DeleteMapping("/user/{id}")
     public Iterable<User> deleteuser(@PathVariable("id") long id) {
         userRepository.deleteById(id);
