b13b74dde132178222543d481c54d72ddbd54f58
denwell76 <danielnieminen04@gmail.com>
2025-04-30 18:04:51 +0300

    Userin muokkaus ja poisto lisätty

--- src/main/java/k25tiimi1backend/k25tiimi1backend/web/UserController.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/web/UserController.java
@@ -3,6 +3,7 @@ package k25tiimi1backend.k25tiimi1backend.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
@@ -26,6 +27,30 @@ public class UserController {
         model.addAttribute("user", new User());
         return "adduser";
     }
+
+    @GetMapping("/edituser/{id}")
+    public String editUserForm(@PathVariable("id") Long userId, Model model) {
+    User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
+    model.addAttribute("user", user);
+    return "edituser";
+}
+
+@PostMapping("/updateuser/{id}")
+public String updateUser(@PathVariable("id") Long userId, User updatedUser) {
+    User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + userId));
+    user.setEmail(updatedUser.getEmail());
+    user.setFirstname(updatedUser.getFirstname());
+    user.setLastname(updatedUser.getLastname());
+    user.setPassword(updatedUser.getPassword());
+    userRepository.save(user);
+    return "redirect:/userlist";
+}
+
+@GetMapping("/deleteuser/{id}")
+public String deleteUser(@PathVariable("id") Long userId) {
+    userRepository.deleteById(userId);
+    return "redirect:/userlist";
+}
 
     @PostMapping("/saveuser")
     public String saveUser(User user) {
