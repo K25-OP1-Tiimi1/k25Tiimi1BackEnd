2dee224cc5578ceef48d4750e6745bbf72dc5f26
JLampinan <jussitussi16@gmail.com>
2025-05-11 13:31:39 +0300

    Lisätty koko MEDIUM, jotta Rhati käynnityisi. Korjattu Index.html sivun URL front endin etusivulle, jotta se ohjaa github pages -sivulle.

--- src/main/java/k25tiimi1backend/k25tiimi1backend/domain/Size.java
+++ src/main/java/k25tiimi1backend/k25tiimi1backend/domain/Size.java
@@ -4,5 +4,10 @@ public enum Size {
     S,
     M,
     L,
-    ONE_KG
+    ONE_KG,
+
+    // EI SAA KÄYTTÄÄ TUOTTEITA LUODESSA/LISÄTESSÄ! LISÄTTY KOSKA RAHTI EI MUUTEN KÄYNNISTY
+    // syystä tai toisesta, kun Rahti käynnistää appia se haluaa koon MEDIUM M sijasta, vaikka tälläsitä 
+    // ei missään tarvita 
+    MEDIUM
 }
